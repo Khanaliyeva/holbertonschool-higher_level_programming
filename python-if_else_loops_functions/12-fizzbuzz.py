4d39489ad055a41229bfcb2c490d6203a7bf4ecc
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-06-18 08:50:49 +0000

    12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+  for digit in range(0, 100):
+    if digit%3 == 0 :
+        print( "Fizz ", end="")
+    elif digit%5 == 0 :
+        print( "Buzz ", end="")
+    elif digit%3 == 0 and digit%5 == 0:
+        print( "FizzBuzz ", end="")
+    else:
+        print('{} '.format(digit), end="")
