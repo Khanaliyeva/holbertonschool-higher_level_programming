7907e85f26dd64c7c62acd9f6c28962da18927df
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-02 05:37:04 +0000

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Defines class BaseGeometry with method area that raises Exception.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raise an Exception because area() is not implemented"""
+        raise Exception("area() is not implemented")
