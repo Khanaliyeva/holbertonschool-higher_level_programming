2131e97663a7e5893be6aadcf559bbe26662da9c
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-02 06:10:30 +0000

    101

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function to add an attribute to an object if possible."""
+"""Function that adds a new attribute to an object if possible."""
 
 
 def add_attribute(obj, attr_name, attr_value):
@@ -7,18 +7,22 @@ def add_attribute(obj, attr_name, attr_value):
 
     Args:
         obj (object): The object to add attribute to.
-        attr_name (str): The name of the attribute.
-        attr_value (any): The value of the attribute.
+        attr_name (str): The attribute name.
+        attr_value (any): The attribute value.
 
     Raises:
-        TypeError: If the object does not allow new attributes.
+        TypeError: If the object can't have new attributes.
     """
-    if hasattr(obj, '__slots__'):
-        # If __slots__ is defined and attr_name not in slots, cannot add attribute
-        if attr_name not in obj.__slots__:
+    if not hasattr(obj, '__dict__'):
+        # If no __dict__, maybe __slots__ defines allowed attributes
+        if not hasattr(obj, '__slots__'):
             raise TypeError("can't add new attribute")
-    try:
-        setattr(obj, attr_name, attr_value)
-    except AttributeError:
-        # Since try/except is not allowed per instructions, let's check in advance if setattr works
-        raise TypeError("can't add new attribute")
+        else:
+            slots = obj.__slots__
+            if isinstance(slots, str):
+                slots = (slots,)
+            if attr_name not in slots:
+                raise TypeError("can't add new attribute")
+
+    # If no exception raised, add the attribute
+    object.__setattr__(obj, attr_name, attr_value)
