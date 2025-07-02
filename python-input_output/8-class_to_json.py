fd7e656c224f19d09499fecdd83a04fc0b370fd7
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-02 06:34:05 +0000

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python3
+
 def class_to_json(obj):
     """Return the dictionary description with simple data structure
     for JSON serialization of an object.
-
     Args:
         obj (object): Instance of a class.
-
     Returns:
         dict: Dictionary of all attributes of obj.
     """
