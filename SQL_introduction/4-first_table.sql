71f9460aaabef41f4cbe64e0073e6c1dade731b5
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-03 08:41:32 +0000

    4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,7 @@
 --creates a table called first_table
-CREATE TABLE first_table (
-	id INT,
-	name VARCHAR(256)
+USE hbtn_0c_0;
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
