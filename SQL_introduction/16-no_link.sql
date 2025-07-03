e2efeec78f8cc3f27ed8f4de015d3beb7ab8b116
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-03 10:26:06 +0000

    16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table
-SELECT sccore, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
