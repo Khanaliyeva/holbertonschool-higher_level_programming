e7e43276c33fe2fcb697f10098b864d20dfb1c78
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-03 09:38:56 +0000

    7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- inserts a new row in the table first_table
-USE hbrn_0c_0;
-INSERT INTO frist_table (id, name) VALUES (89, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
