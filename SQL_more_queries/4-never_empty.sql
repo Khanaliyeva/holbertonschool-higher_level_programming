4ecf91ff3c0c96cacc564b46d876aa365c475533
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-07-03 10:58:26 +0000

    4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+    `id`   INT          DEFAULT 1,
+    `name` VARCHAR(256)
 );
